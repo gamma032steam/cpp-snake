24fe994edf3f0642337beed2672b200941594259
ra314 <rahulgandharva20122000@gmail.com>
2022-10-16 17:31:54 +1100

    Added square struct.

---
+++ square.cc
@@ -0,0 +1,26 @@
+const int board_size = 8;
+
+struct Square {
+  int x;
+  int y;
+  
+  Square operator+(const Square s1) const{
+    return Square{s1.x+x, s1.y+y};
+  }
+  
+  bool in_board() const {
+    return !(x<0 || y<0 || x>=board_size || y>=board_size);
+  }
+  
+  Square(int x, int y): x(x), y(y) {}
+  
+  // Input format is "1,2"
+  Square(std::string square) {
+    assert(square.size() == 3);
+    assert(std::isdigit(square[0]));
+    assert(square[1] == ",");
+    assert(std::isdigit(square[2]));
+    x = std::tostring(square[0])
+    y = std::tostring(square[0])
+  }
+};
