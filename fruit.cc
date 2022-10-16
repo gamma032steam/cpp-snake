e2874cebeadb416e53d7bcad92058a9b3ea98d23
ra314 <rahulgandharva20122000@gmail.com>
2022-10-16 18:23:00 +1100

    Added some randomization code.

--- fruit.cc
+++ fruit.cc
@@ -1,25 +1,33 @@
 #include "constants.cc"
 #include "square.cc"
 
+#include <set>
+
 class Fruit {
   private:
     Square location;
     int TTL;
-    void reappear();
-  public:
+    void reappear(std::set<Square>& snake_squares) {
+      
+    }
     char square_char;
-    
+  public:
     void disappear() {
       square_char = empty_square_char;
     }
     bool is_visible() const {
       return square_char == fruit_char;
     }
-    void tick() {
+    
+    void tick(std::set<Square>& snake_squares) {
       TTL--;
       if (TTL==0) {
         TTL = fruit_max_TTL;
-        reappear();
+        reappear(snake_squares);
       }
+    }
+    
+    const Square& get_loc() const {
+      return location;
     }
 };
--- square.cc
+++ square.cc
@@ -2,37 +2,45 @@
 #include <ctype.h>
 #include <string>
 #include <cassert>
+#include <random>
 
-struct Square {
-  int x;
-  int y;
+class Square {
+  public:
+    int x;
+    int y;
+    
+    Square operator+(const Square s1) const{
+      return Square{s1.x+x, s1.y+y};
+    }
+    
+    operator std::string() const {
+      return std::to_string(x) + "," + std::to_string(y);
+    }
+    
+    bool operator==(const Square& s1) {
+      return s1.x == x && s1.y == y;
+    }
+    
+    bool in_board() const {
+      return !(x<0 || y<0 || x>=board_size || y>=board_size);
+    }
+    
+    Square(int x, int y): x(x), y(y) {}
   
-  Square operator+(const Square s1) const{
-    return Square{s1.x+x, s1.y+y};
-  }
-  
-  operator std::string() const {
-    return std::to_string(x) + "," + std::to_string(y);
-  }
-  
-  bool operator==(const Square& s1) {
-    return s1.x == x && s1.y == y;
-  }
-  
-  bool in_board() const {
-    return !(x<0 || y<0 || x>=board_size || y>=board_size);
-  }
-  
-  Square(int x, int y): x(x), y(y) {}
-  
-  // Input expected is : "1,2"
-  // TODO Accept multidigit nums
-  Square(std::string square) {
-    assert(square.size() == 3);
-    assert(std::isdigit(square[0]));
-    assert(square[1] == ',');
-    assert(std::isdigit(square[2]));
-    x = int(square[0]-'0');
-    y = int(square[2]-'0');
-  }
+    // Input expected is : "1,2"
+    // TODO Accept multidigit nums
+    Square(std::string square) {
+      assert(square.size() == 3);
+      assert(std::isdigit(square[0]));
+      assert(square[1] == ',');
+      assert(std::isdigit(square[2]));
+      x = int(square[0]-'0');
+      y = int(square[2]-'0');
+    }
+    
+    static Square get_random() {
+      int x = rand() % (board_size+1);
+      int y = rand() % (board_size+1);
+      return {x,y};
+    }
 };
