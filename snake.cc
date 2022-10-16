97a4947a0da339c0cca3e694162de9c9f171c91e
ra314 <rahulgandharva20122000@gmail.com>
2022-10-16 18:51:13 +1100

    Implemented most functions for snake.

--- snake.cc
+++ snake.cc
@@ -1,31 +1,51 @@
 #include <cassert>
+#include <deque>
+#include <set>
+#include "square.cc"
 
 class Snake 
 {
   private:
-    std::queue<Square> squares;
-    std::set<Square> squares;
+    std::deque<Square> squares_deque;
+    std::set<Square> squares_set;
     Square dir;
+    void grow() {
+      // Add beyond head square
+      Square head = squares_deque.back() + dir;
+      squares_deque.push_back(head);
+      squares_set.insert(head);
+    }
+    void move() {
+      // Remove tail square
+      squares_set.erase(squares_deque.front());
+      squares_deque.pop_front();
+      // Add beyond head square
+      grow();
+    }
   public:
-    void grow();
-    void move();
-    void change_dir(char dir) {
-      switch(dir) {
+    void change_dir(char new_dir) {
+      switch(new_dir) {
         case 'w':
-          dir = {0,-1}
+          dir = {0,-1};
           break;
         case 'a':
-          dir = {-1,0}
+          dir = {-1,0};
           break;
         case 's':
-          dir = {0,1}
+          dir = {0,1};
           break;
         case 'd':
-          dir = {1,0}
+          dir = {1,0};
           break;
         default:
           assert(false);
       }
     }
-    void tick();
-}
+    void tick(Square& fruit_loc, bool fruit_is_visible) {
+      if ((squares_deque.front() == fruit_loc) && fruit_is_visible) {
+        grow();
+      } else {
+        move();
+      }
+    }
+};
